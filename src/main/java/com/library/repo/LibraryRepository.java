4df1528a96981e7b83de7137eb9f77c38146193a
Kevin <kwang44@buffalo.edu>
2021-04-12 00:00:27 -0400

    Add library config

---
+++ src/main/java/com/library/repo/LibraryRepository.java
@@ -0,0 +1,10 @@
+package com.library.repo;
+
+import com.library.models.Library;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibraryRepository extends CrudRepository<Library, String> {
+
+}
