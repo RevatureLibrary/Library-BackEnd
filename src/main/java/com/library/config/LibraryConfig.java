29fb73db1f3a7c1e14e1ac174daa02c55c26e888
pgonzalez017 <42748202+pgonzalez017@users.noreply.github.com>
2021-04-20 16:45:07 -0400

    Library Config added

---
+++ src/main/java/com/library/config/LibraryConfig.java
@@ -0,0 +1,86 @@
+package com.library.config;
+
+import com.library.models.Department;
+import com.library.models.Library;
+import com.library.models.User;
+import com.library.models.enums;
+import com.library.services.DepartmentService;
+import com.library.services.LibraryService;
+import com.library.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.annotation.PostConstruct;
+import java.sql.Time;
+
+@Configuration
+public class LibraryConfig {
+
+    final private LibraryService libraryService;
+    final  private UserService userService;
+    final  private DepartmentService departmentService;
+
+    @Autowired
+    public LibraryConfig(LibraryService libraryService, @Lazy UserService userService, DepartmentService departmentService) {
+        this.libraryService = libraryService;
+        this.userService = userService;
+        this.departmentService = departmentService;
+    }
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        seedDepartmentTable();
+        seedLibraryTable();
+        seedUsersTable();
+
+    }
+
+    private void seedDepartmentTable() {
+        departmentService.createDepartment( new Department(0, "Unsorted", null));
+        departmentService.createDepartment( new Department(0, "Horror", null));
+        departmentService.createDepartment( new Department(0, "Romance", null));
+        departmentService.createDepartment( new Department(0, "Comedy", null));
+        departmentService.createDepartment( new Department(0, "Sci-Fi", null));
+        departmentService.createDepartment( new Department(0, "Fantasy", null));
+        departmentService.createDepartment( new Department(0, "Self-Help", null));
+        departmentService.createDepartment( new Department(0, "Non-Fiction", null));
+
+    }
+
+    public void seedLibraryTable() {
+        String libraryName = "William Memorial Library";
+
+        if (libraryService.getLibrary() != null) {
+            Time openingTime = Time.valueOf("00:00:00");
+            Time closingTime = Time.valueOf("23:59:59");
+            boolean isOpen = true;
+            Library library = new Library(1, libraryName, openingTime, closingTime, isOpen);
+            libraryService.update(library);
+        }
+    }
+
+
+    private void seedUsersTable() {
+        String sql = "SELECT username, email FROM users U WHERE U.username = \"admin\" OR U.email = \"test@test.com\" LIMIT 1";
+        User u = userService.readByUsername("admin");
+        //List<User> u = (List<User>) entityManagerFactory().getObject().getCurrentSession().createQuery(sql,User.class).uniqueResult();
+        if(u == null ) {
+            User user = new User();
+            user.setFirstName("Test");
+            user.setLastName("Admin");
+            user.setUsername("admin");
+            user.setEmail("test@test.com");
+            user.setPassword("pass");
+            user.setAccountType(enums.AccountType.ADMIN);
+//                user.setConfirmEmail(true);
+            userService.save(user);
+//                logger.info("Users Seeded");
+        } else {
+
+            System.out.println("Admin exists");
+//                logger.info("Users Seeding Not Required");
+        }
+    }
+}
